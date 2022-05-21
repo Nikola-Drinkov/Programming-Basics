8e1dabaf86e46e2d2a5e25463db7e1d466368ff7
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-05-21 14:14:24 +0300

    Completed task from the Conditional Statements Advanced unit.

---
+++ src/CondStatements_Advanced/PersonalTitles_04.java
@@ -0,0 +1,29 @@
+package CondStatements_Advanced;
+
+import java.util.Scanner;
+
+public class PersonalTitles_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double age = Double.parseDouble(sc.nextLine());
+        String gender = sc.nextLine();
+
+        if(age<16){
+            if(gender.equals("m")){
+                System.out.println("Master");
+            }
+            else{
+                System.out.println("Miss");
+            }
+        }
+        else{
+            if(gender.equals("m")){
+                System.out.println("Mr.");
+            }
+            else{
+                System.out.println("Ms.");
+            }
+        }
+    }
+}
