53300a2faed8aa64bfc959f1a1a561e48ff1deaf
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-05-21 14:43:28 +0300

    Completed task from the Conditional Statements Advanced unit.

---
+++ src/CondStatements_Advanced/CinemaTicket_08.java
@@ -0,0 +1,28 @@
+package CondStatements_Advanced;
+
+import java.util.Scanner;
+
+public class CinemaTicket_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String day = sc.nextLine();
+        int price = 0;
+
+        switch(day){
+            case "Monday":
+            case "Tuesday":
+            case "Friday":
+                System.out.println(12);
+                break;
+            case "Wednesday":
+            case "Thursday":
+                System.out.println(14);
+                break;
+            case "Saturday":
+            case "Sunday":
+                System.out.println(16);
+                break;
+        }
+    }
+}
