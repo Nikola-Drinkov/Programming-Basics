bfc1234b1a548c9d240605e48f3d90fbc16f61fc
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-05-28 10:38:30 +0300

    Completed task from the For Loop unit.

---
+++ src/ForLoops/NumbersFrom1to100_01.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class NumbersFrom1to100_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
