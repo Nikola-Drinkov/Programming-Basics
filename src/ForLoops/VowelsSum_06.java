a4205bcb48d6756a752e01cbfab2064b9b54126d
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-05-29 15:04:08 +0300

    Completed task from the For Loop unit.

---
+++ src/ForLoops/VowelsSum_06.java
@@ -0,0 +1,32 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class VowelsSum_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+        int points = 0;
+        for(int i=0; i<text.length(); i++){
+            switch (text.charAt(i)){
+                case'a':
+                    points+=1;
+                    break;
+                case'e':
+                    points+=2;
+                    break;
+                case'i':
+                    points+=3;
+                    break;
+                case'o':
+                    points+=4;
+                    break;
+                case'u':
+                    points+=5;
+                    break;
+            }
+        }
+        System.out.println(points);
+    }
+}
