f8f736948962a3fe96df55aabadc91a147cdebb3
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-05-29 15:30:10 +0300

    Completed task from the For Loop Exercise unit.

---
+++ src/ForLoops_Exercise/NumbersEndingIn7_01.java
@@ -0,0 +1,15 @@
+package ForLoops_Exercise;
+
+import java.util.Scanner;
+
+public class NumbersEndingIn7_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<1000; i++){
+            if(i%10==7){
+                System.out.println(i);
+            }
+        }
+    }
+}
