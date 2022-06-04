50f6a70a9d2b09c24a9d244ccd06e644b9597d7a
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-06-04 12:37:27 +0300

    Completed task from the While Loop unit.

---
+++ src/WhileLoop/MinNumber_07.java
@@ -0,0 +1,20 @@
+package WhileLoop;
+
+import java.util.Scanner;
+
+public class MinNumber_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        int min  = Integer.MAX_VALUE;
+        while (!input.equals("Stop")){
+            int currentNum  = Integer.parseInt(input);
+            if(currentNum<min){
+                min = currentNum;
+            }
+            input = sc.nextLine();
+        }
+        System.out.println(min);
+    }
+}
