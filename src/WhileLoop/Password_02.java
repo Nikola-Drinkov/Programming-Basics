20244fa1e28d921230e35c328c2c4c62f1721196
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-06-04 13:42:03 +0300

    Completed task from the While Loop unit.

---
+++ src/WhileLoop/Password_02.java
@@ -0,0 +1,17 @@
+package WhileLoop;
+
+import java.util.Scanner;
+
+public class Password_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String username = sc.nextLine();
+        String password = sc.nextLine();
+        String input = sc.nextLine();
+        while(!input.equals(password)){
+            input = sc.nextLine();
+        }
+        System.out.printf("Welcome %s!",username);
+    }
+}
