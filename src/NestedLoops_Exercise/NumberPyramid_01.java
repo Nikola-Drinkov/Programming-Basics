3f048dbb43cdacdd77cba4bde46c6ffa3f123e16
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-06-18 17:27:19 +0300

    Completed task from the Nested Loops unit.

---
+++ src/NestedLoops_Exercise/NumberPyramid_01.java
@@ -0,0 +1,22 @@
+package NestedLoops_Exercise;
+
+import java.util.Scanner;
+
+public class NumberPyramid_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        int number = 1;
+        for(int rows=1; rows<=n; rows++){
+            for (int cols = 1; cols<=rows; cols++){
+                if(number>n){
+                    break;
+                }
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
